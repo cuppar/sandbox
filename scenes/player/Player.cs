6451dc9889d330838a93c255acb779c971645a26
cuppar <cuppar@163.com>
2025-02-02 20:20:22 +0800

    实现八向移动逻辑

--- scenes/player/Player.cs
+++ scenes/player/Player.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using Godot;
 using Sandbox.Classes;
+using Sandbox.Globals.Extensions;
 
 namespace Sandbox;
 
@@ -39,19 +41,20 @@ public partial class Player : CharacterBody2D, IStateMachine<Player.State>
     public State GetNextState(State currentState, out bool keepCurrent)
     {
         keepCurrent = false;
-        var moveDirection = GetMoveDirection();
+        var eMoveDirection = GetEMoveDirection();
+        GD.Print($"{eMoveDirection}");
 
         switch (currentState)
         {
             case State.Idle:
-                if (moveDirection.Length() != 0)
+                if (eMoveDirection != EMoveDirection.None)
                 {
                     return State.Move;
                 }
 
                 break;
             case State.Move:
-                if (moveDirection.Length() == 0)
+                if (eMoveDirection == EMoveDirection.None)
                 {
                     return State.Idle;
                 }
@@ -64,16 +67,112 @@ public partial class Player : CharacterBody2D, IStateMachine<Player.State>
 
         keepCurrent = true;
         return currentState;
+    }
 
-        Vector2 GetMoveDirection()
+    public void TickPhysics(State currentState, double delta)
+    {
+        switch (currentState)
+        {
+            case State.Idle:
+                break;
+            case State.Move:
+                _handleMoveTick(delta);
+                break;
+        }
+    }
+
+    #endregion
+
+    #region 移动状态
+
+    #region MoveSpeed
+
+    private float _moveSpeed = 300;
+
+    [Export]
+    public float MoveSpeed
+    {
+        get => _moveSpeed;
+        set => SetMoveSpeed(value);
+    }
+
+    private async void SetMoveSpeed(float value)
+    {
+        await this.EnsureReadyAsync();
+        _moveSpeed = value;
+    }
+
+    #endregion
+
+
+    #region 移动方向相关
+
+    private enum EMoveDirection
+    {
+        Right,
+        RightDown,
+        Down,
+        LeftDown,
+        Left,
+        LeftUp,
+        Up,
+        RightUp,
+        None,
+    }
+
+    private readonly Dictionary<EMoveDirection, Vector2> _moveDirectionMap = new()
+    {
+        { EMoveDirection.None, Vector2.Zero },
+        { EMoveDirection.Right, Vector2.Right },
+        { EMoveDirection.RightDown, new Vector2(1, 1).Normalized() },
+        { EMoveDirection.Down, Vector2.Down },
+        { EMoveDirection.LeftDown, new Vector2(-1, 1).Normalized() },
+        { EMoveDirection.Left, Vector2.Left },
+        { EMoveDirection.LeftUp, new Vector2(-1, -1).Normalized() },
+        { EMoveDirection.Up, Vector2.Up },
+        { EMoveDirection.RightUp, new Vector2(1, -1).Normalized() },
+    };
+
+    private static EMoveDirection GetEMoveDirection()
+    {
+        var moveDirectionInput = GetMoveDirectionInput();
+        if (moveDirectionInput.Length() == 0)
+            return EMoveDirection.None;
+
+        var angle = moveDirectionInput.Angle();
+        const float pi = (float)Math.PI;
+
+        return angle switch
+        {
+            >= -1f / 8 * pi and < 1f / 8 * pi => EMoveDirection.Right,
+            >= 1f / 8 * pi and < 3f / 8 * pi => EMoveDirection.RightDown,
+            >= 3f / 8 * pi and < 5f / 8 * pi => EMoveDirection.Down,
+            >= 5f / 8 * pi and < 7f / 8 * pi => EMoveDirection.LeftDown,
+            >= 7f / 8 * pi or < -7f / 8 * pi => EMoveDirection.Left,
+            >= -7f / 8 * pi and < -5f / 8 * pi => EMoveDirection.LeftUp,
+            >= -5f / 8 * pi and < -3f / 8 * pi => EMoveDirection.Up,
+            >= -3f / 8 * pi and < -1f / 8 * pi => EMoveDirection.RightUp,
+            _ => EMoveDirection.None,
+        };
+
+        Vector2 GetMoveDirectionInput()
         {
             return Input.GetVector("move_left", "move_right", "move_up", "move_down");
         }
     }
 
-    public void TickPhysics(State currentState, double delta)
+    #endregion
+
+    #region 移动Tick
+
+    private void _handleMoveTick(double delta)
     {
+        var direction = _moveDirectionMap[GetEMoveDirection()];
+        Velocity = MoveSpeed * direction;
+        MoveAndSlide();
     }
+
+    #endregion
 
     #endregion
 
